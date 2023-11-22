9b2df72ff7bb412b8fbb103ec9eea12895a2a22b
stclamp <whatisclamp@gmail.com>
2023-11-22 09:44:58 +0200

    add navbar

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -19,7 +19,7 @@ const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
       <div className={cn('flex items-center justify-center flex-col', headingFont.className)}>
-        <div className="mb-4 flex items-center border shadow-small p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task management
         </div>
